a75dd4e75052402332cd2ac6ba66b01db32fa7a9
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2022-08-11 11:52:22 +0200

    Created the rocket and the background, working on the landscape

--- MyProject.cpp
+++ MyProject.cpp
@@ -1,14 +1,15 @@
 // This has been adapted from the Vulkan tutorial
 
 #include "MyProject.hpp"
+#define GLM_FORCE_RADIANS
+#define FLM_FORCE_DEPTH_ZERO_TO_ONE
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
 
 // The uniform buffer object used in this example
 struct UniformBufferObject {
 	alignas(16) glm::mat4 model;
-	alignas(16) glm::mat4 view;	alignas(16) glm::mat4 proj;
+	alignas(16) glm::mat4 view;
+	alignas(16) glm::mat4 proj;
 };
 
 
@@ -24,22 +25,26 @@ class MyProject : public BaseProject {
 	Pipeline P1;
 
 	// Models, textures and Descriptors (values assigned to the uniforms)
-	Model M1;
-	Texture T1;
-	DescriptorSet DS1;
+	Model Rocket_body;
+	Texture Rocket_texture;
+	DescriptorSet DS_Rocket;
+
+	Model Landscape;
+	Texture Landscape_texture;
+	DescriptorSet DS_Landscape;
 	
 	// Here you set the main application parameters
 	void setWindowParameters() {
 		// window size, titile and initial background
 		windowWidth = 800;
 		windowHeight = 600;
-		windowTitle = "My Project";
-		initialBackgroundColor = {0.0f, 0.0f, 0.0f, 1.0f};
+		windowTitle = "Rocket Simulator";
+		initialBackgroundColor = {0.0f, 1.0f, 1.0f, 1.0f};
 		
 		// Descriptor pool sizes
-		uniformBlocksInPool = 1;
-		texturesInPool = 1;
-		setsInPool = 1;
+		uniformBlocksInPool = 2;
+		texturesInPool = 2;
+		setsInPool = 2;
 	}
 	
 	// Here you load and setup all your Vulkan objects
@@ -60,9 +65,9 @@ class MyProject : public BaseProject {
 		P1.init(this, "shaders/vert.spv", "shaders/frag.spv", {&DSL1});
 
 		// Models, textures and Descriptors (values assigned to the uniforms)
-		M1.init(this, MODEL_PATH);
-		T1.init(this, TEXTURE_PATH);
-		DS1.init(this, &DSL1, {
+		Rocket_body.init(this, "models/R27-Ready.obj");
+		Rocket_texture.init(this, "textures/R27 Texture.png");
+		DS_Rocket.init(this, &DSL1, {
 		// the second parameter, is a pointer to the Uniform Set Layout of this set
 		// the last parameter is an array, with one element per binding of the set.
 		// first  elmenet : the binding number
@@ -70,15 +75,27 @@ class MyProject : public BaseProject {
 		// third  element : only for UNIFORMs, the size of the corresponding C++ object
 		// fourth element : only for TEXTUREs, the pointer to the corresponding texture object
 					{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
-					{1, TEXTURE, 0, &T1}
+					{1, TEXTURE, 0, &Rocket_texture}
 				});
+
+		Landscape.init(this, "models/mount.obj");
+		Landscape_texture.init(this, "textures/ground.jpg");
+		DS_Landscape.init(this, &DSL1, {
+						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
+						{1, TEXTURE, 0, &Landscape_texture}
+			});
 	}
 
 	// Here you destroy all the objects you created!		
 	void localCleanup() {
-		DS1.cleanup();
-		T1.cleanup();
-		M1.cleanup();
+		DS_Rocket.cleanup();
+		Rocket_texture.cleanup();
+		Rocket_body.cleanup();
+
+		DS_Landscape.cleanup();
+		Landscape_texture.cleanup();
+		Landscape.cleanup();
+
 		P1.cleanup();
 		DSL1.cleanup();
 	}
@@ -91,21 +108,37 @@ class MyProject : public BaseProject {
 		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
 				P1.graphicsPipeline);
 				
-		VkBuffer vertexBuffers[] = {M1.vertexBuffer};
-		// property .vertexBuffer of models, contains the VkBuffer handle to its vertex buffer		VkDeviceSize offsets[] = {0};
+		VkBuffer vertexBuffers[] = { Rocket_body.vertexBuffer};
+		// property .vertexBuffer of models, contains the VkBuffer handle to its vertex buffer
+		VkDeviceSize offsets[] = {0};
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
-		// property .indexBuffer of models, contains the VkBuffer handle to its index buffer		vkCmdBindIndexBuffer(commandBuffer, M1.indexBuffer, 0,
+		// property .indexBuffer of models, contains the VkBuffer handle to its index buffer
+		vkCmdBindIndexBuffer(commandBuffer, Rocket_body.indexBuffer, 0,
 								VK_INDEX_TYPE_UINT32);
 
 		// property .pipelineLayout of a pipeline contains its layout.
 		// property .descriptorSets of a descriptor set contains its elements.
 		vkCmdBindDescriptorSets(commandBuffer,
 						VK_PIPELINE_BIND_POINT_GRAPHICS,
-						P1.pipelineLayout, 0, 1, &DS1.descriptorSets[currentImage],
+						P1.pipelineLayout, 0, 1, &DS_Rocket.descriptorSets[currentImage],
 						0, nullptr);
 						
-		// property .indices.size() of models, contains the number of triangles * 3 of the mesh.		vkCmdDrawIndexed(commandBuffer,
-					static_cast<uint32_t>(M1.indices.size()), 1, 0, 0, 0);
+		// property .indices.size() of models, contains the number of triangles * 3 of the mesh.
+		vkCmdDrawIndexed(commandBuffer,
+					static_cast<uint32_t>(Rocket_body.indices.size()), 1, 0, 0, 0);
+	
+		//Landscape part, maybe let's find better names
+		VkBuffer vertexBuffers2[] = { Landscape.vertexBuffer };
+		VkDeviceSize offsets2[] = { 0 };
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers2, offsets2);
+		vkCmdBindIndexBuffer(commandBuffer, Landscape.indexBuffer, 0,
+			VK_INDEX_TYPE_UINT32);
+		vkCmdBindDescriptorSets(commandBuffer,
+			VK_PIPELINE_BIND_POINT_GRAPHICS,
+			P1.pipelineLayout, 0, 1, &DS_Landscape.descriptorSets[currentImage],
+			0, nullptr);
+		vkCmdDrawIndexed(commandBuffer,
+			static_cast<uint32_t>(Landscape.indices.size()), 1, 0, 0, 0);
 	}
 
 	// Here is where you update the uniforms.
@@ -118,22 +151,32 @@ class MyProject : public BaseProject {
 					
 					
 		UniformBufferObject ubo{};
-		ubo.model = glm::rotate(glm::mat4(1.0f),
-								time * glm::radians(90.0f),
-								glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f),
 							   glm::vec3(0.0f, 0.0f, 0.0f),
 							   glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.proj = glm::perspective(glm::radians(45.0f),						swapChainExtent.width / (float) swapChainExtent.height,
+		ubo.proj = glm::perspective(glm::radians(45.0f),
+						swapChainExtent.width / (float) swapChainExtent.height,
 						0.1f, 10.0f);
 		ubo.proj[1][1] *= -1;
 		
 		void* data;
 
-		// Here is where you actually update your uniforms
-		vkMapMemory(device, DS1.uniformBuffersMemory[0][currentImage], 0,
-							sizeof(ubo), 0, &data);		memcpy(data, &ubo, sizeof(ubo));
-		vkUnmapMemory(device, DS1.uniformBuffersMemory[0][currentImage]);
+		// Rocket body
+		ubo.model = glm::scale(glm::mat4(1.0f), glm::vec3(0.3f,0.3f, 0.3f));
+		vkMapMemory(device, DS_Rocket.uniformBuffersMemory[0][currentImage], 0,
+							sizeof(ubo), 0, &data);
+		memcpy(data, &ubo, sizeof(ubo));
+		vkUnmapMemory(device, DS_Rocket.uniformBuffersMemory[0][currentImage]);
+
+		//Landscape positioning
+		const float ang = glm::radians(90.0f);
+		glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -5.0f, 0.0f));
+		glm::mat4 rotY = glm::rotate(glm::mat4(1.0f), ang , glm::vec3(0.0f, -1.0f, 0.0f));
+		ubo.model = glm::mat4(1.0f) * rotY * trans;
+		vkMapMemory(device, DS_Landscape.uniformBuffersMemory[0][currentImage], 0,
+			sizeof(ubo), 0, &data);
+		memcpy(data, &ubo, sizeof(ubo));
+		vkUnmapMemory(device, DS_Landscape.uniformBuffersMemory[0][currentImage]);
 	}	
 };
 
